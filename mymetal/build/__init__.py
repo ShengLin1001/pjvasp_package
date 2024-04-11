b9ed7d2b2e0d7ef90fbc39aa903e944bed660a25
ShengLin1001 <cumt_junpei@163.com>
2024-04-12 00:56:18 +0800

    update __init__.py

--- mymetal/build/__init__.py
+++ mymetal/build/__init__.py
@@ -1,11 +1,15 @@
-from mymetal.build.findprim import (my_find_prim, move_atoms, check_direction, cal_area)
+from mymetal.build.findprim import (my_find_prim, move_atoms, check_direction)
 from mymetal.build.hydroxyl import add_hydro_atoms
 from mymetal.build.stretch import (generate_film, stretch_list_along_direction_to_cell)
-from mymetal.build.findhetero import (find_hetero, set_length, magnitude)
+from mymetal.build.findhetero import (find_hetero, my_plot_results, my_find_hetero, set_length, magnitude)
+from mymetal.build.extrfilm import (my_extr_lattice, my_extr_thick, my_extr_etot, cal_area)
+from mymetal.build.findcubic import (find_cubic)
 
-__all__ = ['my_find_prim', 'move_atoms', 'check_direction', 'cal_area', 
+
+__all__ = ['my_find_prim', 'move_atoms', 'check_direction',
            'add_hydro_atoms',
             'generate_film', 'stretch_list_along_direction_to_cell',
-            'find_hetero', 'set_length', 'magnitude'
-
+            'find_hetero', 'my_plot_results', 'my_find_hetero', 'set_length', 'magnitude',
+            'my_extr_lattice', 'my_extr_thick', 'my_extr_etot', 'cal_area',
+            'find_cubic'
 ]
