e5d707c2a3cb2f5aa66a30d432685cc4d055048b
ShengLin1001 <cumt_junpei@163.com>
2024-06-08 23:07:35 +0300

    update

--- mymetal/ml/__init__.py
+++ mymetal/ml/__init__.py
@@ -1,4 +1,8 @@
 from mymetal.ml.dataset import (CustomDataset, get_all_data, get_mean_std)
+from mymetal.ml.plot import (my_imshow)
+from mymetal.ml.model import (train_model, visualize_model, visualize_model_predictions)
 
-__all__ = ['CustomDataset', 'get_all_data', 'get_mean_std'
+__all__ = ['CustomDataset', 'get_all_data', 'get_mean_std',
+           'my_imshow',
+           'train_model', 'visualize_model', 'visualize_model_predictions'
 ]
---
+++ mymetal/ml/plot.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def my_imshow(inp, title=None):
+    """Display image for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))  # 转换为 NumPy 数组并调整维度顺序
+    inp = inp.squeeze()  # 移除单通道
+    mean = np.array([0.5])
+    std = np.array([0.5])
+    inp = std * inp + mean  # 还原归一化
+    inp = np.clip(inp, 0, 1)  # 确保数值在 0 和 1 之间
+    plt.imshow(inp, cmap='gray')  # 显示灰度图像
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    plt.show()  # 显示图像
