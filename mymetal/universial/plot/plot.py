54afb0168a89c45e8853112d496fd61fcb080bb2
ShengLin1001 <cumt_junpei@163.com>
2024-11-24 19:45:49 +0800

    Update plot.py

--- mymetal/universial/plot/plot.py
+++ mymetal/universial/plot/plot.py
@@ -17,6 +17,8 @@ from typing import List, Tuple, Union
 from matplotlib.ticker import MultipleLocator, AutoMinorLocator
 from brokenaxes import brokenaxes
 from matplotlib.ticker import MaxNLocator
+from matplotlib.colors import to_rgb
+
 
 def my_plot(
     one_fig_wh: List[float] = [10.72, 8.205],    
@@ -195,7 +197,7 @@ def my_plot_brokenaxed(
     plt.rcParams['legend.labelspacing'] = 0.5
     plt.rcParams['legend.columnspacing'] = 0.5
 
-    fig_wh = [a * b for a, b in zip(one_fig_wh, fig_subp)]
+    fig_wh = fig_subp[1]*one_fig_wh[0], fig_subp[0]*one_fig_wh[1]
     # inch 10.72* 8.205 is one figsize
     left = left
     right = one_fig_wh[0]-left-axes_width
@@ -217,7 +219,6 @@ def my_plot_brokenaxed(
         axis.tick_params(which='major', direction='in', length=8, width=3.0, pad = tick_pad)
         axis.tick_params(which='minor', direction='in', length=4, width=3.0, pad = tick_pad)
 
-
     def general_modify_legend(legend):
         legend.get_frame().set_boxstyle("Square", pad=0.5)
         legend.get_frame().set_linewidth(2.5)
@@ -225,6 +226,112 @@ def my_plot_brokenaxed(
         legend.get_frame().set_facecolor('white')
         legend.get_frame().set_alpha(1)
 
+    def general_margin_bin(axis,
+                            y_margin = 0.1, 
+                            y_nbins = 3,
+                            x_margin = 0.1, 
+                            x_nbins = 4, 
+                            prune = 'both',
+                            if_autoscale = True):
+        axis.margins(x=x_margin, y=y_margin)
+        axis.yaxis.set_major_locator(MaxNLocator(nbins=y_nbins, prune=prune))
+        axis.xaxis.set_major_locator(MaxNLocator(nbins=x_nbins, prune=prune))
+        if if_autoscale:
+            # Force axis limits to update based on the new margins
+            axis.autoscale(enable=True, axis='both', tight=False)
+    
+    # 将修改图例函数绑定到 fig 对象，方便调用
     fig.general_modify_legend = general_modify_legend
+    fig.general_margin_bin = general_margin_bin
 
     return fig, ax
+
+def my_plot_energy_components(fig_subp: List[int]=[4,4],
+                                one_fig_wh: List[float] = [11.72, 8.205],    
+                                fig_sharex: bool = False, 
+                                grid: bool = True, 
+                                labelpad: int = 15, 
+                                tick_pad: int = 10, 
+                                left: float = 1.918, 
+                                top: float = 0.9517,
+                                axes_height: float = 5.89,
+                                axes_width: float = 7.31,
+                                dic_list: list=None, 
+                                label_list: list=None,
+                                color_list: list=[to_rgb('#fc9d9a'),to_rgb('#8dc6ff')],
+                                xlabel: str="Directory",
+                                poly_fit_e: int = 3,
+                                fit_count: int = 100,
+                                color: str = 'tab:red',
+                                atoms_number: int = 1,
+                                if_tight_layout: bool= False,
+                                vline_list: list= [],
+                                vline_colors: list= []):
+    fig_subp = fig_subp
+    fig, axes = my_plot(one_fig_wh, fig_subp, fig_sharex, grid, labelpad, tick_pad, 
+                        left, top, axes_height, axes_width)
+
+    tagsref=[
+        "Directory", "TOTEN", "PSCENC", "TEWEN", "DENC", "EXHF",
+        "XCENC", "Double1", "Double2", "Double", "EENTRO", "EBANDS", "EATOM", "Ediel_sol"
+    ]
+    tags = [tag.lower() for tag in tagsref]
+    tags = tags[1:]
+    print(tags)
+
+    i = 0
+    j = 0
+    count = 0
+    for tag in tags:
+        count = count+1
+        ax = axes[i, j]
+        #print(count, i,j, tag)
+
+        coeff_list=[]
+        xmin = []
+        xmax = []
+        for i, dic in enumerate(dic_list):
+            x = dic['directory']
+            xmin.append(min(x))
+            xmax.append(max(x))
+            y = dic[tag]/atoms_number
+            coeff_list.append(np.polyfit(x, y, poly_fit_e))
+            ax.plot(x, y, marker='o', linestyle='', color = color_list[i], label=label_list[i])
+        ax.set_xlabel(f'{xlabel}')
+        ax.set_ylabel(f'{tag.upper()}'+' (eV/atom)')
+        #ax.set_xlim(-0.05, 0.4)
+        ax.grid(True, linestyle='--', linewidth=2)
+        yfit_list=[]
+        xfit = np.linspace(min(xmin), max(xmax), fit_count)
+        for i, coeff in enumerate(coeff_list):
+            poly = np.poly1d(coeff)
+            yfit = poly(xfit)
+            yfit_list.append(yfit)
+            ax.plot(xfit, yfit, linestyle='--', color = color_list[i])
+        #ax.axvline(x=2.79338179, color=color, linestyle='--', linewidth=3)
+        if len(dic_list) > 1:
+            diff = yfit_list[1] - yfit_list[0]
+            diff_label = rf"$E_{{\mathrm{{{label_list[1]}}}}}-E_{{{label_list[0]}}}$"
+            ax2 = ax.twinx() 
+            ax2.set_ylabel(diff_label+' (eV/atom)', color=color, labelpad = 15)
+            ax2.plot(xfit, diff, 'o', color = color, 
+                    label=diff_label, markeredgecolor=color) 
+            ax2.axhline(y=0, color=color, linestyle='--', linewidth=3)
+            ax2.tick_params(axis='y', labelcolor=color, direction='in', color = color)
+            ax2.spines['right'].set_color(color) 
+            ax2.tick_params(which='major', direction='in', color = color, length=8, width=3.0, pad = 10)
+            ax2.tick_params(which='minor', direction='in', color = color, length=4, width=3.0, pad = 10)
+            ax2.set_zorder(ax.get_zorder() - 1)
+            ax.spines['right'].set_visible(False)
+        fig.general_modify_legend(ax.legend(loc='upper center', bbox_to_anchor=(0.5, 0.95)))
+        ax.set_title(f'{tag.upper()}', fontsize=28)
+        #ax.text(0.4, 0.9, f'{tag.upper()}', transform=ax.transAxes, fontsize=28)
+        for i, vline in enumerate(vline_list):
+            ax.axvline(x=vline, color=vline_colors[i], linestyle='--', linewidth=3)
+        i=count//fig_subp[1]
+        j=count%fig_subp[1]
+        if count==(fig_subp[1]*fig_subp[0]):
+            break
+    if if_tight_layout:
+        fig.tight_layout()
+    return fig, axes
