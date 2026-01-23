413eb1fb692204f661f87721e449d53337aeac7e
ShengLin1001 <cumt_junpei@163.com>
2026-01-23 22:10:21 +0800

    feat(DFT): :sparkles: 添加了尹老师写的建模函数，并fix了prism1函数return None的bug

--- mymetal/build/bulk/create.py
+++ mymetal/build/bulk/create.py
@@ -34,6 +34,8 @@ from ase.lattice.cubic import FaceCenteredCubic, BodyCenteredCubic
 from ase.lattice.hexagonal import HexagonalClosedPacked
 from mymetal.universal.atom.moveatom import move_atoms
 from ase import Atoms
+import numpy as np
+from myvasp import vasp_func as vf
 
 
 # FCC (111) Plane
@@ -88,7 +90,7 @@ def create_hcp_prism1(a: float = None, c: float = None, size: tuple = (1, 1, 1),
     elif mode == 'narrow':
         atoms = move_atoms(atoms, [0,0,-0.05*a], if_scale_position=False)
     atoms.wrap()
-    return None
+    return atoms
 
 # HCP (10-11) Prism II
 def create_hcp_prism2(a: float = None, c: float = None, size: tuple = (1, 1, 1),
@@ -108,3 +110,208 @@ def create_hcp_prism2(a: float = None, c: float = None, size: tuple = (1, 1, 1),
 
 # todo: BCC (110) Plane
 
+
+##### This part is taken from 
+# https://github.com/BinglunYin/myalloy_package/blob/master/myvasp/vasp_create_hcp.py
+
+#===================
+# examples:
+
+# a = 3.23415
+# ca = 1.5992
+
+## For pry1 and prism1:
+##      bp: 33 for W, -33 for N
+
+# vasp_create_hcp_basal(a, ca, np.array([1, 1, 10]) )
+# vasp_create_hcp_prism1(a, ca, np.array([1, 1, 16]), bp = 33)
+# vasp_create_hcp_pyr1(a, ca, np.array([1, 1, 16]), bp=-33)
+# vasp_create_hcp_pyr1(a, ca, np.array([1, 1, 16]), bp=33)
+# vasp_create_hcp_pyr2(a, ca, np.array([1, 1, 10]) )
+
+def vasp_create_hcp_basal(a, ca, ncell, bp=33):
+    print('==> create hcp basal plane: ')
+    print(a, ca, ncell, bp)
+
+    latt = np.array([
+        [1.0, 0, 0],
+        [-0.5, np.sqrt(3)/2, 0],   
+        [0, 0, ca],
+    ]) * a
+    
+    motif = np.array([
+        [0, 0, 0],
+        [1/3,  2/3,  1/2],
+    ])
+
+    atoms = vf.create_supercell(latt, motif, ncell)
+
+    if bp == 33:
+        atoms.positions = atoms.positions + np.array([0, 0, 0.1])*a
+        atoms.wrap()
+
+    atoms.pos_a0 = a 
+    return atoms
+    #vf.my_write_vasp(atoms, filename='POSCAR', vasp5=True)
+
+def vasp_create_hcp_basal_ortho(a, ca, ncell, bp=33):
+    print('==> create hcp basal_ortho plane: ')
+    print(a, ca, ncell, bp)
+
+    latt = np.array([
+        [ 1.0,          0,  0],
+        [   0, np.sqrt(3),  0],   
+        [   0,          0, ca],
+    ]) * a
+    
+    motif = np.array([
+        [0.0,    0,    0],
+        [1/2,  1/2,    0],
+        [  0,  2/6,  1/2],
+        [1/2,  5/6,  1/2],
+    ])
+
+    atoms = vf.create_supercell(latt, motif, ncell)
+    
+    if bp == 33:
+        atoms.positions = atoms.positions + np.array([0, 0, 0.1])*a
+        atoms.wrap()
+
+    atoms.pos_a0 = a 
+    return atoms
+    #vf.my_write_vasp(atoms, filename='POSCAR', vasp5=True)
+
+def vasp_create_hcp_prism1(a, ca, ncell, bp=33):
+    print('==> create hcp prism plane: ')
+    print(a, ca, ncell, bp)
+
+    ncell2 = ncell.copy()
+    for i in np.arange(3):
+        ncell2[i] = ncell[ np.mod(i+2, 3) ]
+    
+    atoms = vasp_create_hcp_basal(a, ca, ncell2, bp=0)
+
+    #atoms = vf.my_read_vasp('POSCAR')
+    atoms = vf.make_SFP_xy(atoms, i1=1)
+    atoms = vf.make_a3_ortho(atoms)
+
+    if bp == 33:
+        print('==> create prism1-W')
+        atoms.positions = atoms.positions + np.array([0, 0, 0.1])*a
+        atoms.wrap()
+    
+    elif bp == -33:
+        print('==> create prism1-N')
+        atoms.positions = atoms.positions + np.array([0, 0, -0.1])*a
+        atoms.wrap()
+    
+    atoms.pos_a0 = a 
+    return atoms
+    #vf.my_write_vasp(atoms, filename='POSCAR', vasp5=True)
+
+def vasp_create_hcp_pyr1(a, ca, ncell, bp=33):
+    print('==> create hcp basal pyr1: ')
+    print(a, ca, ncell, bp)
+
+    latt = np.array([
+        [ 1.0, 0, 0],
+        [ 0.5, np.sqrt(3)/2, 0],   
+        [-1.0, 0, ca],
+    ]) * a
+    
+    motif = np.array([
+        [0, 0, 0],
+        [1/6, 2/3, 1/2],
+    ])
+
+    ncell2 = ncell.copy()
+    for i in np.arange(3):
+        ncell2[i] = ncell[ np.mod(i+2, 3) ]
+   
+    atoms = vf.create_supercell(latt, motif, ncell2)
+    atoms = vf.make_SFP_xy(atoms, i1=1)
+    atoms = vf.make_a3_ortho(atoms)
+
+    if bp == 33:
+        print('==> create pry1-W')
+        atoms.positions = atoms.positions + np.array([0, 0, 0.1])*a
+        atoms.wrap()
+
+    elif bp == -33:
+        print('==> create pry1-N')
+        atoms.positions = atoms.positions + np.array([0, 0, -0.05])*a
+        atoms.wrap()
+    
+        
+    atoms.pos_a0 = a 
+    return atoms
+    # vf.my_write_vasp(atoms, filename='POSCAR', vasp5=True)
+
+def vasp_create_hcp_pyr2(a, ca, ncell, bp=33):
+    print('==> create hcp basal pyr2: ')
+    print(a, ca, ncell, bp)
+
+    latt = np.array([
+        [ 1.0, 0, 0],
+        [ 0.0, np.sqrt(3), 0],   
+        [-1.0, 0, ca],
+    ]) * a
+    
+    motif = np.array([
+        [  0,    0,    0],
+        [1/2,  1/2,    0],
+        [  0,  5/6,  1/2],
+        [1/2,  1/3,  1/2]
+    ])
+
+    ncell2 = ncell.copy()
+    for i in np.arange(3):
+        ncell2[i] = ncell[ np.mod(i+2, 3) ]
+   
+    atoms = vf.create_supercell(latt, motif, ncell2)
+    atoms = vf.make_SFP_xy(atoms, i1=1)
+    atoms = vf.make_a3_ortho(atoms)
+    
+
+    if bp == 33:
+        atoms.positions = atoms.positions + np.array([0, 0, 0.1])*a
+        atoms.wrap()
+
+    atoms.pos_a0 = a 
+    return atoms
+    #vf.my_write_vasp(atoms, filename='POSCAR', vasp5=True)
+
+##### End of code taken from
+
+
+##### This part is taken from 
+# https://github.com/BinglunYin/myalloy_package/blob/master/myvasp/vasp_create_fcc.py
+
+def vasp_create_fcc_100(a, ncell, bp=33):
+    print('==> create fcc 100 plane: ')
+    print(a, ncell, bp)
+
+    latt = np.array([
+        [1.0, 0, 0],
+        [0, 1.0, 0],   
+        [0, 0, 1.0],
+    ]) * a
+   
+    motif = np.array([
+        [0, 0, 0],
+        [0.5,  0.5,  0],
+        [0.5,  0,  0.5],
+        [0,  0.5,  0.5],
+    ])
+
+    atoms = vf.create_supercell(latt, motif, ncell)
+    atoms.pos_a0 = a 
+
+    if bp == 33:
+        atoms.positions = atoms.positions + np.array([0, 0, 0.1]) *a 
+        atoms.wrap()
+
+    return atoms
+    #vf.my_write_vasp(atoms, filename='POSCAR', vasp5=True)
+
+##### End of code taken from
