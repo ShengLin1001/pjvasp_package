1b0b0385addd6e420b0fdb035ff41d3ca0fd2d55
Louis <cumt_junpei@163.com>
2026-08-30 11:32:39 -0700

    docs(api): :memo: 补充 cal_density docstring

--- mymetal/universal/atom/density.py
+++ mymetal/universal/atom/density.py
@@ -12,6 +12,17 @@ Functions:
 from ase import Atoms
 
 def cal_density(atoms: Atoms) -> float:
+    """Calculate the atomic number density of a structure.
+
+    Args:
+        atoms (Atoms): An ``ase.Atoms`` object with a non-degenerate cell.
+
+    Returns:
+        float: Number density in atoms/Å³.
+
+    Raises:
+        Exception: If the cell is degenerate (volume ≈ 0), ASE raises.
+    """
     volume = atoms.get_volume()  # in Å^3
     natoms = len(atoms)
     density = natoms / volume  # in atoms/Å^3
